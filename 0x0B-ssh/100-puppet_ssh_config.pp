643fc52f9fd7042969ef0d90140c27e5f319de5c
YaredShisema <yaredshi3@gmail.com>
2023-11-22 03:22:59 -0800

    update ssh

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
+#!/usr/bin/env bash
+# using puppet to make changes to our configuration file
 # set up your client SSH configuration file so that you can connect
 # to a server without typing a password.
 
-include stdlib
-file_line {'change private key':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/school'
-}
-file_line {'no password':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
-}
+file { '/etc/ssh/ssh_config':
+	ensure => present,
+
+content =>"
+
+	#SSH client configuration
+	host*
+	IdentityFile ~/.ssh/school
+	passwordAuthentication no
+	",
+} 
